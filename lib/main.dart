ef6057ac2819eae0431785d3e283c55f89caad1a
med0o0o <modyshwiat2@gmail.com>
2025-08-01 14:21:05 +0300

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('لحظتنا')),
+      body: Center(child: Text('أهلا يا أليسيو 🦋')),
+    ),
+  ));
+}
